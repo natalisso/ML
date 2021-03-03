0612a2e63ff9162e9c3e9f27326dda7b392dd026
Natalia Souza Soares <nss2@cin.ufpe.br>
2021-03-03 14:07:22 -0300

    Add execution time measurement

--- Lista1/main.py
+++ Lista1/main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import time 
 from utils import(
     data_pre_processing,
     targets_pre_processing,
@@ -22,33 +23,33 @@ for mode in modes:
     results_file = os.path.join(RESULTS_DIR, f"results_{mode}.txt")
 
     for dataset_id, dataset_name in enumerate(datasets):
+        logger_mode = "a"
         if dataset_id == 0:
             logger_mode = "w+"
-        else:
-            logger_mode = "a"
-
-        # Getting the dataset and its infos
-        dataset_file = os.path.join(DATASETS_DIR, dataset_name+".csv")
-        dataset = pd.read_csv(dataset_file)             
-        num_attributes = len(dataset.columns)
-        indices = [i for i in dataset.index]  
-
-        # Pre-processing the data and targets
-        X = dataset.iloc[:, 0:num_attributes-1].to_numpy()
-        X = data_pre_processing(X)
-        y = dataset.iloc[:, num_attributes-1:]
-        y = targets_pre_processing(y)
-    
-        # Generating the k-folds
-        num_folds = 10
-        k_folds = k_fold_cross_validation(num_folds, indices) 
 
         with open(results_file, logger_mode) as logger:
             logger.write(f"Dataset: {dataset_name}\n")
 
+            # Getting the dataset and its infos
+            dataset_file = os.path.join(DATASETS_DIR, dataset_name+".csv")
+            dataset = pd.read_csv(dataset_file)             
+            num_attributes = len(dataset.columns)
+            indices = [i for i in dataset.index]  
+
+            # Pre-processing the data and targets
+            X = dataset.iloc[:, 0:num_attributes-1].to_numpy()
+            X = data_pre_processing(X)
+            y = dataset.iloc[:, num_attributes-1:]
+            y = targets_pre_processing(y)
+        
+            # Generating the k-folds
+            num_folds = 10
+            k_folds = k_fold_cross_validation(num_folds, indices) 
+
             scores = []
             n_neighbors = [1,2,3,5,7,9,11,13,15]
             for k in n_neighbors:
+                start_time = time.time()
                 for i in range(num_folds):
                     # Generating the training and test data and targets
                     training_data, X_test, y_test = split_data(k_folds, X, y)
@@ -57,5 +58,7 @@ for mode in modes:
                     preditions = k_nearest_neighbors(training_data, X_test, k, mode)
                     accuracy = get_accuracy(y_test, preditions)
                     scores.append(accuracy)
-                logger.write('Accuracy (k = {}): {:.3f}%\n'.format(k, sum(scores)/float(len(scores))))
+                total_accuracy = sum(scores)/float(len(scores))
+                logger.write("Execution Time (k = {}): {} seconds\n".format(k, time.time() - start_time))
+                logger.write("Accuracy (k = {}): {:.3f}%\n".format(k, total_accuracy))
             logger.write("\n")
