ffe5fc95f3092babbeb251ec458c91e2791d4c37
Natalia Souza Soares <nss2@cin.ufpe.br>
2021-03-24 02:56:53 -0300

    Add baseline w/ knn

--- lista_2/main.py
+++ lista_2/main.py
@@ -8,7 +8,7 @@ from LVQ import train_codebooks
 PARENT_PATH = os.path.dirname(__file__)
 ROOT_PATH = os.path.abspath(os.path.join(PARENT_PATH, os.pardir))
 DATASETS_DIR =  os.path.join(ROOT_PATH, "datasets")
-RESULTS_DIR = os.path.join(PARENT_PATH, "results/LVQ3")
+RESULTS_DIR = os.path.join(PARENT_PATH, "results")
 SEED = 2
 
 sys.path.insert(1, ROOT_PATH)
@@ -27,64 +27,73 @@ w = 0.3
 epsilon = 0.05
 all_n_codebooks = [5, 10, 15]
 all_n_neighbors = [1, 3]
-
+versions = {"0": "knn", "1": "LVQ1", "2.1": "LVQ2", "3": "LVQ3"}
 
 for dataset_id, dataset_name in enumerate(datasets):
-    results_filename = os.path.join(RESULTS_DIR, f"results_{dataset_name}.txt")
-    results_file = open(results_filename, "w+")
 
-    for n_neighbors in all_n_neighbors:
-        results_file.write("Number of Neighbors: " + str(n_neighbors) + "\n")
+    for version in versions.keys():
+        results_dir = os.path.join(RESULTS_DIR, versions[version])
+        results_filename = os.path.join(results_dir, f"results_{dataset_name}.txt")
+        results_file = open(results_filename, "w+")
+        for n_neighbors in all_n_neighbors:
+            results_file.write("Number of Neighbors: " + str(n_neighbors) + "\n")
 
-        for n_codebooks in all_n_codebooks:
-            # Getting the dataset and its infos
-            dataset_file = os.path.join(DATASETS_DIR, dataset_name+".csv")
-            dataset = pd.read_csv(dataset_file)             
-            num_attributes = len(dataset.columns)
+            for n_codebooks in all_n_codebooks:
+                # Getting the dataset and its infos
+                dataset_file = os.path.join(DATASETS_DIR, dataset_name+".csv")
+                dataset = pd.read_csv(dataset_file)             
+                num_attributes = len(dataset.columns)
 
-            # Getting and processing the data and targets
-            X = dataset.iloc[:, 0:num_attributes-1].to_numpy()
-            y = dataset.iloc[:, num_attributes-1:]
-            X = data_pre_processing(X)
-            y = targets_pre_processing(y)
+                # Getting and processing the data and targets
+                X = dataset.iloc[:, 0:num_attributes-1].to_numpy()
+                y = dataset.iloc[:, num_attributes-1:]
+                X = data_pre_processing(X)
+                y = targets_pre_processing(y)
 
-            # Gettig the k folds from all the data
-            raw_data = [data + target for data, target in zip(X,y)]
+                # Gettig the k folds from all the data
+                raw_data = [data + target for data, target in zip(X,y)]
 
-            # Getting cookbooks vectors - LVQ1 + LVQ2.1
-            data_LVQ1 = train_codebooks(raw_data, n_codebooks, learn_rate, n_epochs)
-            new_data = train_codebooks(data_LVQ1, n_codebooks, learn_rate, n_epochs, version="3", w=w, epsilon=epsilon)
+                if version != "0":
+                # Getting cookbooks vectors -> LVQ1, LVQ1 + LVQ2.1 or LVQ1 + LVQ3
+                    data_LVQ1 = train_codebooks(raw_data, n_codebooks, learn_rate, n_epochs)
+                    if version != "1":
+                        data = train_codebooks(data_LVQ1, n_codebooks, learn_rate, n_epochs, version=version, w=w, epsilon=epsilon)
+                    else:
+                        data = data_LVQ1
+                else:
+                    data = raw_data
 
-            # Gettig the k folds from the new training data
-            folds = cross_validation_split(new_data, n_folds)
 
-            scores = []
-            predictions = []
-            targets = []
+                # Gettig the k folds from the training data
+                folds = cross_validation_split(data, n_folds)
 
-            for fold in folds:
-                train_set = list(folds)
-                train_set.remove(fold)
-                train_set = sum(train_set, [])
-                test_set = list()
+                scores = []
+                predictions = []
+                targets = []
 
-                for row in fold:
-                    row_copy = list(row)
-                    test_set.append(row_copy)
-                    row_copy[-1] = None
-                
-                predicted = k_nearest_neighbors(train_set, test_set, n_neighbors)
-                actual = [row[-1] for row in fold]
-                accuracy = get_accuracy(actual, predicted)
-                scores.append(accuracy)
-                predictions.extend(predicted)
-                targets.extend(actual)
+                for fold in folds:
+                    train_set = list(folds)
+                    train_set.remove(fold)
+                    train_set = sum(train_set, [])
+                    test_set = list()
 
-            total_accuracy = sum(scores)/float(len(scores))
-            results_file.write("Accuracy (n_codebooks = {}) = {:.3f}%\n".format(n_codebooks, total_accuracy))
-            results_file.write("y_actual = " + str(targets) + "\n")
-            results_file.write("y_pred = " + str(predictions) + "\n\n")
-            plot_subtitle = "Num Protótipos = " + str(n_codebooks) + ", Num Vizinhos mais próximos = " + str(n_neighbors)
-            plot_file = os.path.join(RESULTS_DIR, f"plots/{dataset_name}_p={n_codebooks}_k={n_neighbors}.png")
-            confusion_matrix(targets, predictions, dataset_name, plot_subtitle, plot_file)
-            print('Mean Accuracy: %.3f%%' % (total_accuracy))
+                    for row in fold:
+                        row_copy = list(row)
+                        test_set.append(row_copy)
+                        row_copy[-1] = None
+                    
+                    predicted = k_nearest_neighbors(train_set, test_set, n_neighbors)
+                    actual = [row[-1] for row in fold]
+                    accuracy = get_accuracy(actual, predicted)
+                    scores.append(accuracy)
+                    predictions.extend(predicted)
+                    targets.extend(actual)
+
+                total_accuracy = sum(scores)/float(len(scores))
+                results_file.write("Accuracy (n_codebooks = {}) = {:.3f}%\n".format(n_codebooks, total_accuracy))
+                results_file.write("y_actual = " + str(targets) + "\n")
+                results_file.write("y_pred = " + str(predictions) + "\n\n")
+                plot_subtitle = "Num Protótipos = " + str(n_codebooks) + ", Num Vizinhos mais próximos = " + str(n_neighbors)
+                plot_file = os.path.join(results_dir, f"plots/{dataset_name}_p={n_codebooks}_k={n_neighbors}.png")
+                confusion_matrix(targets, predictions, dataset_name, plot_subtitle, plot_file)
+                print('Mean Accuracy: %.3f%%' % (total_accuracy))
